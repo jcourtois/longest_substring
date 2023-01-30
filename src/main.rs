53ec95105292e1830747e97b0ce8cb9661e30c8b
James Courtois <jcourtois@gmail.com>
2023-01-29 21:58:46 -0500

    solved it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+use std::collections::{HashSet, LinkedList};
+
 fn main() {
     println!("Hello, world!");
 }
@@ -7,8 +10,30 @@ struct Solution {}
 
 #[cfg(test)]
 impl Solution {
-    pub fn longest_substring(s: &str) -> &str {
-        s
+    pub fn longest_substring(s: String) -> i32 {
+        let mut max: usize = 0;
+        let mut list: LinkedList<char> = LinkedList::new();
+        let mut set: HashSet<char> = HashSet::new();
+
+        for c in s.chars() {
+            if set.contains(&c) {
+                while let Some(front) = list.pop_front() {
+                    if front == c {
+                        list.push_back(c);
+                        max = std::cmp::max(max, list.len());
+                        break;
+                    } else {
+                        set.remove(&front);
+                    }
+                }
+            } else {
+                set.insert(c);
+                list.push_back(c);
+                max = std::cmp::max(max, list.len());
+            }
+        }
+
+        return max as i32;
     }
 }
 
@@ -18,16 +43,16 @@ mod tests {
 
     #[test]
     fn case1() {
-        assert_eq!(Solution::longest_substring("abcabcbb"), "abc")
+        assert_eq!(Solution::longest_substring(String::from("abcabcbb")), 3)
     }
 
     #[test]
     fn case2() {
-        assert_eq!(Solution::longest_substring("bbbbb"), "b")
+        assert_eq!(Solution::longest_substring(String::from("bbbbb")), 1)
     }
 
     #[test]
     fn case3() {
-        assert_eq!(Solution::longest_substring("pwwkew"), "wke") 
+        assert_eq!(Solution::longest_substring(String::from("pwwkew")), 3)
     }
 }
